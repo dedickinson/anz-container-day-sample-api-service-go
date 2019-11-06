dc848fbacd99f6e14740037ebda72c9790b699f5
Duncan Dickinson <dedickinson@users.noreply.github.com>
2019-11-06 11:44:22 +1000

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "All your bases are belong to us."}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
